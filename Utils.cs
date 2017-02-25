316c727e6a9110d16d427317e400b8ab3ea865b7
Jerome PL <jerome-pl2@hotmail.ca>
2017-02-25 08:54:40 -0500

    Fixed: Use static Random object for number generation

--- Utils.cs
+++ Utils.cs
@@ -14,7 +14,6 @@ namespace Neat
         public static double nextGaussian(double mean, double stdDev)
         {
             // See http://stackoverflow.com/a/218600
-            Random rand = new Random(); //reuse this if you are generating many
             double u1 = 1.0 - rand.NextDouble(); //uniform(0,1] random doubles
             double u2 = 1.0 - rand.NextDouble();
             double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2); //random normal(0,1)
